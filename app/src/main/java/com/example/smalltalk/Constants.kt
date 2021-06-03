82ce20e2fd804fb1ee04de4509dbeb8d9c76d2dc
joeych8 <73697534+joeych8@users.noreply.github.com>
2021-06-03 23:06:37 +0200

    removed dashboard fragment

--- app/src/main/java/com/example/smalltalk/Constants.kt
+++ app/src/main/java/com/example/smalltalk/Constants.kt
@@ -6,3 +6,4 @@ const val SHARED_PREFS_USERNAME_KEY = "SHARED_PREFS_USERNAME_KEY"
 const val SHARED_PREFS_FIRSTNAME_KEY = "SHARED_PREFS_FIRSTNAME_KEY"
 
 const val BASE_URL = "https://us-central1-smalltalk-3bfb8.cloudfunctions.net/api/"
+
--- app/src/main/java/com/example/smalltalk/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/smalltalk/ui/profile/ProfileFragment.kt
@@ -1,18 +1,30 @@
 package com.example.smalltalk.ui.profile
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.camera.core.ImageCapture
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.smalltalk.*
 import com.example.smalltalk.login.LoginActivity
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.imageview.ShapeableImageView
+import java.io.File
+import java.util.concurrent.ExecutorService
 
 
 class ProfileFragment : Fragment() {
@@ -20,18 +32,21 @@ class ProfileFragment : Fragment() {
     private lateinit var profileViewModel: ProfileViewModel
     private lateinit var signOutButton: MaterialButton
     private lateinit var textViewUserName: TextView
+    private lateinit var profileImage: ShapeableImageView
 
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
 
         profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
         signOutButton = view.findViewById(R.id.sign_out_button)
         textViewUserName = view.findViewById(R.id.textView_userName)
+        profileImage = view.findViewById(R.id.profile_image)
 
         return view
     }
@@ -42,14 +57,20 @@ class ProfileFragment : Fragment() {
         val user = UserManager.loggedInUser
         textViewUserName.text = user.firstName.capitalize()
 
+        setButtonListeners()
 
 
+    }
+
+
+    private fun setButtonListeners() {
         signOutButton.setOnClickListener {
             (activity as? MainActivity)?.logOutUser()
-//            val intent = Intent(activity, LoginActivity::class.java)
-//            intent.flags =
-//                intent.flags or Intent.FLAG_ACTIVITY_NO_HISTORY //TODO: clears the content once you leave the page. need on sign out?
-//            startActivity(intent)
         }
+
+        profileImage.setOnClickListener {
+
+        }
+
     }
 }
